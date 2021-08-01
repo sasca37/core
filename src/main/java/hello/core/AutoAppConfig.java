b64324b75e7a5278c1bce1f0ebc8aedf384e6d90
sasca37 <sasca37@naver.com>
2021-08-01 17:31:50 +0900

    1

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         //member 패키지 이하만 컴포넌트 스캔의 대상이 된다.
-        basePackages = "hello.core.member",
+        basePackages =   "hello.core.member",
         // 스캔을 안할 대상을 지정 (수동 설정 정보랑 출동 방지)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
